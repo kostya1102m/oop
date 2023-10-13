ff05484ad08ec61b164e99876662e69ca512ddec
mercury <122353133+kostya1102m@users.noreply.github.com>
2023-10-13 20:45:29 +1000

    Update target1.cpp

--- target1.cpp
+++ target1.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
+#include <time.h>
 #include <cmath>
-#include <ctime>
-#include <random>
 using namespace std;
 int main(){
     setlocale(LC_ALL,0);
-    short int sum,points = 0;
-	vector<pair<int,int>> shots(5);
+    short int sum,points,shots = 0;
 	srand(time(0));
-	for (int i = 0; i < 5;i++){
-		cout << "Введите координаты "<< i + 1 <<"-го выстрела"<< endl;
-        cin >> shots[i].first  >> shots[i].second;
-		shots[i].first += rand()%10 - 5;
-		shots[i].second += rand()%10 - 5;
+	while (shots != 5){
+		pair<int,int> shot;
+		cin >> shot.first >> shot.second;
+		shot.first += rand() % 11 - 5;
+		shot.second += rand() % 11 - 5;
+		points = 5 - (int)sqrt(pow(shot.first,2)+pow(shot.second,2));
+		if (((points >> 31 ) & 1) == 0){
+        sum += points;
 	}
-	 for (const auto& shot : shots){
-		 points = 5 - (int)sqrt(pow(shot.first,2)+pow(shot.second,2));;
-        if (((points >> 31) & 1 ) == 0){
-            sum += points;
-			//cout << points <<" "<< sum << endl;
-        };
-	 }
+	shots++;
+	//cout << points <<" "<< sum << endl; 
+	}
 	if (sum < 10) cout << "Лузер" << endl;
-	else cout << "ТЫ ЧЕМПИОН " << endl;
     return 0;
 }
