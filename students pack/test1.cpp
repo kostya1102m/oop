e702e78b8483b58ba52eaed8d195f300211f85b6
mercury <122353133+kostya1102m@users.noreply.github.com>
2024-01-03 00:25:54 +1000

    Update test1.cpp

--- students pack/test1.cpp
+++ students pack/test1.cpp
@@ -2,19 +2,19 @@
 #include <iostream>
 int main()
 {
-    Student s1;
+    Student s1("Петр");
     s1.addMarks(5);
     s1.addMarks(5);
     s1.addMarks(5);
     s1.addMarks(5);
     s1.Otlichnik();
 
-    Student s2;
-    s1.addMarks(5);
-    s1.addMarks(4);
-    s1.addMarks(4);
-    s1.addMarks(4);
-    s1.Otlichnik();
+    Student s2("Маша");
+    s2.addMarks(5);
+    s2.addMarks(4);
+    s2.addMarks(4);
+    s2.addMarks(4);
+    s2.Otlichnik();
 
     return 0;
 
