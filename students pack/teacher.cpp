1877112b96563dfa74ba0d1fdf329fb0d3436e10
mercury <122353133+kostya1102m@users.noreply.github.com>
2024-01-03 17:05:52 +1000

    Update teacher.cpp

--- students pack/teacher.cpp
+++ students pack/teacher.cpp
@@ -1,1 +1,49 @@
+#include "teacher.h"
+#include <iostream>
+
+void Teacher::setMood(Mood m)
+{
+    this->mood = m;
+}
+
+Mood Teacher::getMood()
+{
+    return this->mood;
+}
+
+void Teacher::giveMark(shared_ptr<Student> &student)
+{
+    srand(time(0));
+    short int mark;
+    if (mood == Mood::Great && student->Otlichnik())
+        mark = 5;
+    else if (!student->Otlichnik() && mood == Mood::Good)
+    {
+        if (rand() % 2 == 0)
+            mark = 3;
+        else
+            mark = 4;
+    }
+    else if (!student->Otlichnik() && mood == Mood::Great)
+    {
+        if (rand() % 2 == 0)
+            mark = 4;
+        else
+            mark = 5;
+    }
+    else if (student->Otlichnik() && (mood == Mood::Good || mood == Mood::Terrible))
+    {
+        short int n = rand() % 3 == 0;
+        mark = (n == 0) ? 3 : (n == 1 ? 4 : 5);
+    }
+    else
+    {
+        if (rand() % 2 == 0)
+            mark = 2;
+        else
+            mark = 3;
+    }
+
+    student->addMarks(mark);
+}
 
