ec6f7078aae507ea707631781fadf198025ab70a
mercury <122353133+kostya1102m@users.noreply.github.com>
2024-01-03 22:57:32 +1000

    Update teacher5and2.h

--- students pack/teacher5and2.h
+++ students pack/teacher5and2.h
@@ -4,9 +4,16 @@
 class Teacher5 : public Teacher
 {
 public:
-    void giveMark(shared_ptr<Student>& student) override;
+    Teacher5(string n):Teacher(n){
+        this->markcount = 0;
+    }
+
+    void giveMark(shared_ptr<Student> &student) override;
 };
 class Teacher2 : public Teacher
-{   
-    void giveMark(shared_ptr<Student>& student) override;
+{
+    Teacher2(string n) : Teacher(n) {
+        this->markcount = 0;
+    }
+    void giveMark(shared_ptr<Student> &student) override;
 };
