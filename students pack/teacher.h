4a298eb8f8ab6c2075f14a40d91b863ca388f6ac
mercury <122353133+kostya1102m@users.noreply.github.com>
2024-01-03 22:56:54 +1000

    Update teacher.h

--- students pack/teacher.h
+++ students pack/teacher.h
@@ -14,20 +14,24 @@ enum class Mood
 
 class Teacher
 {
-private:
+protected:
     string name;
     Mood mood;
+    unsigned markcount;//для смены настроения
 
 public:
     Teacher(string n) : name(n)
     {
         srand(time(0));
         mood = static_cast<Mood>(rand() % 3);
+        markcount = 0;
     };
 
     Mood getMood();
 
     virtual void giveMark(shared_ptr<Student> &student);
+
+    unsigned getMarkcount();
 
     void setMood(Mood m);
 };
