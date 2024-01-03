5ca848bd761bab65f7d2b2f2c77f814d87ff0ff9
mercury <122353133+kostya1102m@users.noreply.github.com>
2024-01-03 16:19:53 +1000

    Update teacher.h

--- students pack/teacher.h
+++ students pack/teacher.h
@@ -1,8 +1,29 @@
 #pragma once
+#include <vector>
+#include <iostream>
+#include <ctime>
+#include "student.h"
+#include <memory>
 
-class Teacher{
+enum class Mood{
+    Terrible,
+    Good,
+    Great
+};
 
+class Teacher
+{
+private:
+    string name;
+    Mood mood;
+    
 public:
-  void giveMark(){}
+    Teacher(string n):name(n){};
 
-}
+    Mood getMood();
+
+    void giveMark(shared_ptr<Student>& student);
+
+    void setMood(const Mood m);
+
+};
