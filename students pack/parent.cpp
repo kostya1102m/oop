4918e4afb026af7d1c66c31c9e880c7010814200
mercury <122353133+kostya1102m@users.noreply.github.com>
2024-01-04 17:45:04 +1000

    Update parent.cpp

--- students pack/parent.cpp
+++ students pack/parent.cpp
@@ -5,7 +5,28 @@ private:
     string name;
     vector<shared_ptr<Student>> Children;
     Mood parentmood;
-    bool ChildrenNumberCheck()
+    unsigned parents;
+
+public:
+    Parent(string n, Mood m)
+    {
+        name = n;
+        srand(time(0));
+        parentmood = static_cast<Mood>(rand() % 3);
+    }
+    vector<shared_ptr<Student>> getChild()
+    {
+        return Children;
+    }
+    Mood getparentmood()
+    {
+        return parentmood;
+    }
+    int getparents()
+    {
+        return parents;
+    }
+    virtual bool ChildrenNumberCheck()
     {
         if (this->Children.size() == 0)
         {
@@ -21,7 +42,7 @@ private:
         if (ChildrenNumberCheck())
         {
             for (int i = 0; i < this->Children.size(); i++)
-                otl = Children[i]->getGS() || otl;
+                otl = Children[i]->Otlichnik() || otl;
 
             if (otl == true)
                 return true;
@@ -29,9 +50,7 @@ private:
                 return false;
         }
     }
-
-public:
-    virtual void addChild(shared_ptr<Student> child)
+    void addChild(shared_ptr<Student> child)
     {
         this->Children.push_back(child);
         if (child->getParent() > 2)
@@ -47,7 +66,7 @@ public:
             if (areChildrenExcellent() && parentmood == Mood::Great)
             {
                 for (int i = 0; i < this->Children.size(); i++)
-                    if (Children[i]->getGS())
+                    if (Children[i]->Otlichnik())
                     {
                         Children[i]->GSorNot();
                         cout << "\nЭТОТ ГРЫЗУН КРАСАВА";
@@ -97,7 +116,7 @@ public:
             }
         }
     }
-    virtual void tellAboutOwnSpecificChild(shared_ptr<Student> child)
+    void tellAboutOwnSpecificChild(shared_ptr<Student> child)
     {
         if (ChildrenNumberCheck())
         {
@@ -129,16 +148,81 @@ public:
 
 class Grandparent : public Parent
 {
+private:
+    vector<shared_ptr<Parent>> GrandparentChildren;
+
 public:
+    Grandparent(string n, Mood grandmood) : Parent(n, grandmood) {}
+    void addChild(shared_ptr<Parent> GChild)
+    {
+        this->GrandparentChildren.push_back(GChild);
+        if (GChild->getparents() > 2)
+        {
+            this->GrandparentChildren.pop_back();
+            cout << "У ребенка не может быть больше 2 родителей\n";
+        }
+    }
+    bool ChildrenNumberCheck() override
+    {
+        if (this->GrandparentChildren.size() == 0)
+        {
+            cout << "У этого родителя нет детей(это у бабушки или у дедушки)\n";
+            return false;
+        }
+        else
+            return true;
+    }
     void tellAboutOwnAllChildren() override
     {
+        if (ChildrenNumberCheck())
+        {
+
+            for (int i = 0; i < this->GrandparentChildren.size() - 1; i++)
+            {
+                for (int j = 0; j < this->GrandparentChildren[i]->getChild().size() - 1; j++)
+                {
+                    GrandparentChildren[i]->getChild()[j]->GSorNot();
+                    cout << "Внучок молодец\n";
+                }
+            }
+        }
     }
 
     void tellAboutOwnRandomChild() override
     {
+        if (ChildrenNumberCheck())
+        {
+            srand(time(0));
+            int Parentchildindex = rand() % GrandparentChildren.size() + 1;
+            int childindex = rand() % GrandparentChildren[Parentchildindex]->getChild().size() + 1;
+            if (GrandparentChildren[Parentchildindex]->getChild()[childindex]->Otlichnik())
+                cout << GrandparentChildren[Parentchildindex]->getChild()[childindex]->getName() << " Старики уже не те, за то наши внуки - это будущее\n";
+            else
+                cout << GrandparentChildren[Parentchildindex]->getChild()[childindex]->getName() << " Это еще не конец, пойдем кашки покушаем?\n";
+        }
     }
 
-    void tellAboutOwnSpecificChild(shared_ptr<Student> child) override
+    void tellAboutOwnSpecificChild(shared_ptr<Student> child)
     {
+        if (ChildrenNumberCheck())
+        {
+            for (int i = 0; i < this->GrandparentChildren.size() - 1; i++)
+            {
+                for (int j = 0; j < this->GrandparentChildren[i]->getChild().size() - 1; j++)
+                {
+                    if (child == GrandparentChildren[i]->getChild()[j])
+                    {
+                        if (child->Otlichnik())
+                            cout << child->getName() << " внучок старается изо всех сил, он самый лучший\n";
+                        else
+                            cout << child->getName() << " красавааааааааааа\n";
+                    }
+                }
+            }
+        }
+    }
+    void tellAboutAnotherChild() 
+    {
+
     }
 };
