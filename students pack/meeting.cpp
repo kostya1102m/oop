0dfd54f63de21627b2d2aad01dd63caf5df58b4a
mercury <122353133+kostya1102m@users.noreply.github.com>
2024-01-12 00:09:55 +1000

    stage 9 final update

--- students pack/meeting.cpp
+++ students pack/meeting.cpp
@@ -1,49 +1,67 @@
 #include "parent.cpp"
+#include "lesson.cpp"
 class Meeting
 {
 private:
     vector<shared_ptr<Parent>> parents;
     vector<shared_ptr<Teacher>> teachers;
     vector<shared_ptr<Lesson>> lessons;
-
+    vector<shared_ptr<Student>> LostStudents;
 
 public:
-    Meeting(){}
-    void addLessonToDiscuss(shared_ptr<Lesson> lesson)
-    {
-        lessons.push_back(lesson);
-    }
-    void addParent(shared_ptr<Parent> parent)
-    {
-        parents.push_back(parent);
-    }
-    void addTeacher(shared_ptr<Teacher> teacher)
-    {
-        teachers.push_back(teacher);
-    }
+    Meeting(vector<shared_ptr<Lesson>> &l, vector<shared_ptr<Teacher>> &t, vector<shared_ptr<Parent>> &p) : lessons(l), teachers(t), parents(p) {}
 
+    shared_ptr<Parent> StudentsParent(shared_ptr<Student> student)
+    {
+        for (int i = 0; i < parents.size(); i++)
+        {
+            if (find(parents[i]->getChild().begin(), parents[i]->getChild().end(), student) != parents[i]->getChild().end())
+            {
+                return parents[i];
+            }
+        }
+        return nullptr;
+    }
+    void ListOfLostStudents()
+    {
+        cout << "Список тех студентов, чьи родители отстутстовали на собрании:\n";
+        for (int i = 0; i < LostStudents.size(); i++)
+        {
+            cout << LostStudents[i]->getName() << endl;
+        }
+    }
     void discussLessons()
     {
         for (int k = 0; k < lessons.size(); k++)
         {
-            cout << "Обсуждение про " << k << " -ое занятие,";
-            cout << "проведенное " << lessons[k]->getTeacher()->getName() << endl;
-            for (int i = 0; i < parents.size(); i++)
+
+            cout << "Обсуждение про " << k + 1 << " -ое занятие";
+            if (find(teachers.begin(), teachers.end(), lessons[k]->getTeacher()) == teachers.end())
+                cout << "Учитель " << k + 1 << "-го занятия отствутствует на собрании\n";
+
+            else
             {
-                for (int j = 0; j < parents[i]->getChild().size(); j++)
+                cout << " которое провел - " << lessons[k]->getTeacher()->getName() << endl;
+                for (int j = 0; j < lessons[k]->getStudents().size(); j++)
                 {
-                    if (parents[i]->getChild()[j]->getMarks().size() != 0) //кто получил оценки
+                    if (lessons[k]->getStudents()[j]->getMarks().empty())
+                        cout << lessons[k]->getStudents()[j]->getName() << " не получил оценок за " << k + 1 << " урок\n";
+
+                    else
                     {
-                        if (parents[i]->getChild()[j]->Otlichnik())
+                        shared_ptr<Parent> p = StudentsParent(lessons[k]->getStudents()[j]);
+                        if (p == nullptr)
                         {
-                            cout << parents[i]->getChild()[j]->getName();
-                            parents[i]->tellAboutOwnSpecificChild(parents[i]->getChild()[j]);
+                            if (find(LostStudents.begin(), LostStudents.end(), lessons[k]->getStudents()[j]) == LostStudents.end())
+                                LostStudents.push_back(lessons[k]->getStudents()[j]);
                         }
                         else
-                            cout << parents[i]->getChild()[j]->getName() << " балбесина\n";
+                            p->tellAboutOwnSpecificChild(lessons[k]->getStudents()[j]);
                     }
                 }
             }
         }
+        cout << endl;
+        ListOfLostStudents();
     }
 };
