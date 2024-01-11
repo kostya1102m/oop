07d8123a954a8cfde69511242519afc52e08d917
mercury <122353133+kostya1102m@users.noreply.github.com>
2024-01-12 00:12:47 +1000

    Update teacher.h

--- students pack/teacher.h
+++ students pack/teacher.h
@@ -1,19 +1,11 @@
 #pragma once
-#include <vector>
-#include <iostream>
-#include <ctime>
-#include "student.h"
-#include <memory>
-
 enum class Mood
 {
     Terrible,
     Good,
     Great
 };
-enum class Classes{
-    
-};
+
 
 class Teacher
 {
@@ -23,12 +15,14 @@ protected:
     unsigned markcount;//для смены настроения
 
 public:
+    Teacher(){};
     Teacher(string n) : name(n)
     {
         srand(time(0));
         mood = static_cast<Mood>(rand() % 3);
         markcount = 0;
     };
+
 
     Mood getMood();
 
